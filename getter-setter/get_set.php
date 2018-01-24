9b34c3b61d7a30846f6a9df4b2c5bfe5b9547cb1
Aakash Dhar <aakash.dhar@simteq.com>
2018-01-24 09:27:05 +0530

    Added code and notes

--- getter-setter/get_set.php
+++ getter-setter/get_set.php
@@ -1,1 +1,45 @@
 <?php
+
+  class Person
+  {
+    private $name;
+    private $DateOfBirth;
+
+    public function setName($name)
+    {
+      if (!is_string($name)) {
+        throw new \Exception("this is not a string");
+
+      }
+      $this->name = $name;
+    }
+
+    public function setDateOfBirth($DateOfBirth)
+    {
+      $this->DateOfBirth = $DateOfBirth;
+    }
+
+    public function getName()
+    {
+      return $this->name;
+    }
+
+    public function getDateOfBirth()
+    {
+      return $this->DateOfBirth;
+    }
+  }
+
+
+$aakash = new Person();
+// To throw an error
+// $aakash->setName(1321111531);
+
+$aakash->setName('Aakash');
+$aakash->setDateOfBirth('06-09-1992');
+
+$name = $aakash->getName();
+$dateofbirth = $aakash->getDateOfBirth();
+
+echo $dateofbirth;
+echo $name;
